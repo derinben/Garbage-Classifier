c010cc2a481cf5e01575a85f6c87845a70914ba6
Derin Ben <derinben17@gmail.com>
2020-12-05 13:05:21 +0530

    added files-1

---
+++ model.py
@@ -0,0 +1,140 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img, array_to_img
+import zipfile
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+zip_ref = zipfile.ZipFile('garbagedata.zip', 'r')
+zip_ref.extractall("/split-garbage-dataset")
+zip_ref.close()
+
+
+image_count = 0
+labels = []
+train_counts = []
+for dirname in os.listdir('split-garbage-dataset/train'):
+    labels.append(dirname)
+    image_count = 0
+    for img in os.listdir(os.path.join('split-garbage-dataset/train',dirname)):
+        image_count +=1
+    train_counts.append(image_count)
+
+class_weights = []
+total_samples = train_generator.samples
+total_classes = len(train_generator.class_indices)
+for ele in train_counts:
+    result = round(total_samples / (total_classes * ele),2)
+    class_weights.append(result)
+
+
+class_weights = dict(zip(train_generator.class_indices.values(),class_weights))
+
+
+
+
+# Add our data-augmentation parameters to ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale = 1./255.,
+                                   rotation_range = 40,
+                                   width_shift_range = 0.2,
+                                   height_shift_range = 0.2,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True,
+                                   vertical_flip = True,
+                                  )
+
+# Note that the validation data should not be augmented!
+valid_datagen = ImageDataGenerator( rescale = 1.0/255. )
+test_datagen = ImageDataGenerator( rescale = 1.0/255. )
+
+# Flow training images in batches of 8 using train_datagen generator
+train_generator = train_datagen.flow_from_directory('../input/split-garbage-dataset/train',
+                                                    batch_size =8,
+                                                    class_mode = 'categorical',
+                                                    target_size = (150,150))
+
+# Flow validation images in batches of 4 using valid_datagen generator
+validation_generator = valid_datagen.flow_from_directory( '../input/split-garbage-dataset/valid',
+                                                          batch_size  = 4,
+                                                          class_mode  = 'categorical',
+                                                          target_size = (150,150))
+
+
+
+tf.keras.backend.clear_session()
+tf.random.set_seed(42)
+np.random.seed(42)
+
+xception_model = keras.applications.xception.Xception(weights="imagenet",
+                                                      include_top=False,
+                                                      input_shape = (150,150,3))
+avg = keras.layers.GlobalAveragePooling2D()(xception_model.output)
+output = keras.layers.Dense(6, activation="softmax")(avg)
+model2 = keras.models.Model(inputs=xception_model.input, outputs=output)
+
+
+optimizer = keras.optimizers.Nadam(lr=0.001, beta_1=0.9, beta_2=0.999)
+model2.compile(optimizer =optimizer ,loss = 'categorical_crossentropy',metrics =['accuracy'])
+
+def exponential_decay(lr0, s):
+    def exponential_decay_fn(epoch):
+        return lr0 * 0.1**(epoch / s)
+    return exponential_decay_fn
+
+exponential_decay_fn = exponential_decay(lr0=0.001, s=10)
+
+
+
+early_stopping_cb = tf.keras.callbacks.EarlyStopping(patience = 10,restore_best_weights = True)
+lr_scheduler = keras.callbacks.LearningRateScheduler(exponential_decay_fn)
+mc =tf.keras.callbacks.ModelCheckpoint('XceptionLR_Xce2exp.h5', save_best_only=True)
+class CustomCallBack(tf.keras.callbacks.Callback):
+        def on_epoch_end(self,epoch,logs={}):
+            if(logs.get('accuracy')>0.99):
+                print("\nReached 99.0% accuracy so cancelling training!")
+                self.model.stop_training = True
+
+
+mycallback = CustomCallBack()
+history1 = model2.fit(
+            train_generator,
+            steps_per_epoch=train_generator.samples/train_generator.batch_size,
+            epochs = 30,
+            validation_data = validation_generator,
+            validation_steps= validation_generator.samples/validation_generator.batch_size,
+            callbacks= [early_stopping_cb,mc,lr_scheduler,mycallback],
+            verbose=1)
+
+fig = plt.figure(figsize=(10,10))
+
+# Plot accuracy
+plt.subplot(221)
+plt.plot(history1.history['accuracy'],'bo-', label = "acc")
+plt.plot(history1.history['val_accuracy'], 'ro-', label = "val_acc")
+plt.title("train_accuracy vs val_accuracy")
+plt.ylabel("accuracy")
+plt.xlabel("epochs")
+plt.grid(True)
+plt.legend()
+
+# Plot loss function
+plt.subplot(222)
+plt.plot(history1.history['loss'],'bo-', label = "loss")
+plt.plot(history1.history['val_loss'], 'ro-', label = "val_loss")
+plt.title("train_loss vs val_loss")
+plt.ylabel("loss")
+plt.xlabel("epochs")
+plt.grid(True)
+plt.legend()
+
+plt.subplot(223)
+plt.plot(history1.epoch,history1.history['lr'],'o-')
+plt.title("train_loss vs val_loss")
+plt.ylabel("loss")
+plt.xlabel("epochs")
+plt.grid(True)
+plt.legend()
+
---
+++ predict.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
+
+model2 = keras.models.load_model('XceptionLR_Xce2exp.h5')
+
+# Flow test.py images using test_datagen generator
+test_datagen = ImageDataGenerator( rescale = 1.0/255. )
+test_generator = test_datagen.flow_from_directory('../input/split-garbage-dataset/test.py',
+                                                  batch_size = 1,
+                                                  class_mode = 'categorical',
+                                                  target_size = (150,150),shuffle = False)
+model2.evaluate(test_generator,batch_size = 1)
+
+y_pred = model2.predict(test_generator)
+y_pred = np.argmax(y_pred, axis=1)
+print(classification_report(test_generator.classes, y_pred))
+cf_matrix = confusion_matrix(test_generator.classes, y_pred)
+plt.figure(figsize=(10,10))
+heatmap = sns.heatmap(cf_matrix, xticklabels=test_generator.class_indices.keys(), yticklabels=test_generator.class_indices.keys(), annot=True, fmt='d', color='red')
+plt.xlabel('Predicted class')
+plt.ylabel('True class')
+plt.title('Confusion matrix of model')
